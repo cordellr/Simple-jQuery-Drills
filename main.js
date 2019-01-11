4cdc923ea12628158d1b9e9d4ca83369226a4171
cordellr <cordell.rainock@gmail.com>
2019-01-11 15:05:26 -0600

    Button disabled when no value in box

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-$('#btnSubmit').click(function(){
-    alert('The button has been clicked');
+$(document).ready(function () {
+    $('#btnSubmit').prop('disabled', true);
+    $('#textEntry').keyup(function () {
+        if ($(this).val() != '') {
+            $('#btnSubmit').prop('disabled', false);
+        }
+    });
 });
+
+$('#btnSubmit').click(function () {
+    let textValue = $('#textEntry').val();
+    alert('You entered "' + textValue + '"');
+});
+
